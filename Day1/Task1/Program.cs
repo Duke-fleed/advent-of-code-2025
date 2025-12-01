9e029fe805c8c8328a368fa7022f7e0f6eda37b1
Lasha <amashukeli.lasha@gmail.com>
2025-12-01 21:37:10 +0200

    Day1

---
+++ Day1/Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿var currentPosition = 50;
+var zeroOccurences = 0;
+
+var lines = File.ReadLinesAsync("../input.txt");
+
+await foreach (var line in lines)
+{
+    var direction = line[0];
+    var number = Convert.ToInt32(line[1..]);
+    var factor = direction == 'R' ? 1 : -1;
+    currentPosition += number*factor;
+    currentPosition = (100 + currentPosition)%100;
+
+    if (currentPosition==0)
+        zeroOccurences++;
+}
+
+Console.WriteLine(zeroOccurences);
+
---
+++ Day1/Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿var currentPosition = 50;
+var zeroPasses = 0;
+
+var lines = File.ReadLinesAsync("../input.txt");
+
+await foreach (var line in lines)
+{
+    var trimmedline = line.Trim();
+    var direction = trimmedline[0];
+    var number = Convert.ToInt32(trimmedline[1..]);
+    zeroPasses += number/100;
+    number = number%100;
+    if (direction == 'R')
+    {
+        currentPosition += number;
+        zeroPasses += currentPosition/100;
+        currentPosition %= 100;
+    }
+    else
+    {
+        if ((currentPosition<=number && currentPosition != 0) || currentPosition == number)
+        {
+            zeroPasses++;
+        }
+        currentPosition -= number;
+        currentPosition = (100 + currentPosition)%100;
+    }
+}
+
+Console.WriteLine(zeroPasses);
+
