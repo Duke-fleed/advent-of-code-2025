32d95a116026f2b92a685f4b00902f1b6af37d34
Lasha Amashukeli <lasha.amashukeli@bondora.com>
2025-12-04 12:16:50 +0200

    Day 2 - Task 1

---
+++ Day2/Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿var text = await File.ReadAllTextAsync("../input.txt");
+
+var ranges = text.Split(',');
+long sum = 0;
+
+foreach (var range in ranges)
+{
+    var bounds = range.Split('-').Select(long.Parse).ToArray();
+    var start = bounds[0];
+    var end = bounds[1];
+
+    for (var i = start; i <= end; i++)
+    {
+        var iString = i.ToString();
+        if (iString.Length % 2 == 1)
+            continue;
+        var splitiString = iString.Chunk(iString.Length / 2).ToArray();
+        var number1 = long.Parse(splitiString[0]);
+        var number2 = long.Parse(splitiString[1]);
+        if (number1 == number2)
+        {
+            sum += i;
+        }
+    }
+}
+
+Console.WriteLine(sum);
---
+++ Day2/Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿var text = await File.ReadAllTextAsync("../input.txt");
+
+var ranges = text.Split(',');
+long sum = 0;
+System.Console.WriteLine(ranges.Length);
+foreach (var range in ranges)
+{
+    var bounds = range.Split('-').Select(long.Parse).ToArray();
+    var start = bounds[0];
+    var end = bounds[1];
+
+    for (var i = start; i <= end; i++)
+    {
+        var iString = i.ToString();
+        for (var n = 0; n < iString.Length/2; n++)
+        {
+            var substring1 = iString.Substring(0, n+1);
+        }
+    }
+}
+
+Console.WriteLine(sum);
