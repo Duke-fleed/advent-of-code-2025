7ae1e2c47a7bfa7b8726b6c20354cd486946e63c
Lasha Amashukeli <lasha.amashukeli@bondora.com>
2025-12-05 15:46:52 +0200

    Day 5

---
+++ Day5/Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿var lines = File.ReadLinesAsync("../input.txt");
+
+var ranges = new List<(long start, long end)>();
+var idsToCheck = new List<long>();
+
+await foreach (var line in lines)
+{
+    if (string.IsNullOrWhiteSpace(line))
+        continue;
+
+    if (line.Contains("-"))
+    {
+        var numbers =  line.Split('-');
+        ranges.Add((long.Parse(numbers[0]), long.Parse(numbers[1])));
+    }
+    else
+    {
+        idsToCheck.Add(long.Parse(line));
+    }
+}
+
+var count = 0;
+foreach (var number in idsToCheck)
+{
+    if (ranges.Any(r => number >= r.start && number <= r.end))
+    {
+        count++;
+    }
+}
+Console.WriteLine(count);
---
+++ Day5/Task2/Program.cs
@@ -0,0 +1,41 @@
+﻿var lines = File.ReadLinesAsync("../input.txt");
+
+var ranges = new List<(long start, long end)>();
+
+await foreach (var line in lines)
+{
+    if (string.IsNullOrWhiteSpace(line))
+        continue;
+
+    if (line.Contains("-"))
+    {
+        var numbers =  line.Split('-');
+        ranges.Add((long.Parse(numbers[0]), long.Parse(numbers[1])));
+    }
+    else
+    {
+        break;
+    }
+}
+
+var orderedRanges = ranges.OrderBy(r => r.start).ToList();
+
+long maxNumberAdded = 0;
+long freshIngredientsCount = 0;
+foreach (var range in orderedRanges)
+{
+    if (range.end <= maxNumberAdded)
+        continue;
+
+    if (maxNumberAdded < range.start)
+    {
+        freshIngredientsCount += range.end - range.start + 1;
+        maxNumberAdded = range.end;
+    }
+    if (maxNumberAdded >= range.start && maxNumberAdded < range.end)
+    {
+        freshIngredientsCount += range.end - maxNumberAdded;
+        maxNumberAdded = range.end;
+    }
+}
+Console.WriteLine(freshIngredientsCount);
