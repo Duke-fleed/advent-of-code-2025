fb79929b0a6164357a5849ef7faf9c0e63f89755
Lasha <amashukeli.lasha@gmail.com>
2025-12-06 14:27:19 +0200

    Day 6

---
+++ Day6/Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿var lines = File.ReadLinesAsync("../input.txt");
+
+var numbers = new List<List<long>>();
+var operations = new List<string>();
+var rowIndex = 0;
+await foreach (var line in lines)
+{
+  if (rowIndex<4)
+  {
+   var lineNumbers = new List<long>();
+   var splitNumbers = line.Split(" ")
+    .Where(x=> !string.IsNullOrWhiteSpace(x))
+    .Select(long.Parse)
+    .ToList();
+   numbers.Add(splitNumbers);
+  }
+  else 
+  {
+    operations = [.. line.Split(" ").Where(x=> !string.IsNullOrWhiteSpace(x))]; 
+  }
+  rowIndex ++;
+}
+
+long totalSum = 0;
+for (int i=0; i<numbers[0].Count; i++){
+  totalSum += operations[i] == "+" ? numbers[0][i] + numbers[1][i] + numbers[2][i] + numbers[3][i] : numbers[0][i] * numbers[1][i] * numbers[2][i] * numbers[3][i];
+}
+
+Console.WriteLine(totalSum);
---
+++ Day6/Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿var lines = await File.ReadAllLinesAsync("../input.txt");
+
+ulong totalSum = 0;
+var currentOperation = string.Empty;
+var currentNumbers = new List<ulong>();
+for (int i=0; i<lines[0].Length+1; i++)
+{
+    if (i==lines[0].Length || (lines[0][i] == ' ' && lines[1][i] == ' ' && lines[2][i] == ' ' && lines[3][i] == ' ' && lines[4][i] == ' '))
+    {
+        totalSum += currentOperation == "+" ? currentNumbers.Aggregate((x, y) => x + y) 
+                  : currentNumbers.Aggregate((x, y) => x * y);
+        currentOperation = string.Empty;
+        currentNumbers.Clear();
+    } else
+    {
+        if (string.IsNullOrWhiteSpace(currentOperation))
+        {
+            currentOperation = lines[4][i].ToString();
+        }
+        var currentNumber = lines[0][i].ToString() + lines[1][i].ToString() + lines[2][i].ToString() + lines[3][i].ToString();
+        currentNumbers.Add(ulong.Parse(currentNumber.Trim()));
+    }
+}
+System.Console.WriteLine(totalSum);
